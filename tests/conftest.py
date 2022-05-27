6442b35f1ea03c82ebd8b0ce6d2be252365657b3
dzaslavskiy <dzaslavskiy@easydynamics.com>
2022-05-27 18:38:47 -0400

    fix import in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,21 +6,19 @@ import pytest
 from fastapi import testclient
 
 import db
-from db import SessionLocal
 
 # pylint: disable=wrong-import-position
 sys.modules["gpo.database"] = db
-from gpo.main import app
-from gpo.models import Letter
+from gpo import main, models
 
 
 @pytest.fixture
 def generate_data():
     """test fixture"""
 
-    def get_letter() -> Letter:
+    def get_letter() -> models.Letter:
         """get a letter object"""
-        return Letter(
+        return models.Letter(
             id=5,
             name="Name",
             address="Address",
@@ -41,11 +39,11 @@ def generate_data():
 @pytest.fixture(scope="session")
 def session() -> typing.Generator:
     """session"""
-    yield SessionLocal()
+    yield db.SessionLocal()
 
 
 @pytest.fixture(scope="module")
 def client() -> typing.Generator:
     """api test client"""
-    with testclient.TestClient(app) as test_client:
+    with testclient.TestClient(main.app) as test_client:
         yield test_client
